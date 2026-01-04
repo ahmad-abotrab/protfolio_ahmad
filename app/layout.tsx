293035af4699d6ca063322cca60f6c80cda6fd9a
ahmadtrab <abotrab.ahmad3099@gmail.com>
2026-01-04 18:06:51 +0300

    feat(layout): update favicon to use single png file

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,15 +33,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon.png",
         type: "image/svg+xml",
       },
     ],
